ea800e203a590e1f370181b7cd38aeb9c3cfb7a2
silent-codder <ajinkyashinde9799@gmail.com>
2023-08-30 22:03:40 +0530

    create thread added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,42 @@
-//app/page.tsx
-import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+import { fetchPosts } from "@/lib/actions/thread.action";
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+
+  const result = await fetchPosts(1, 30)
+  const user = await currentUser()
+
   return (
     <div>
+      <h1 className="head-text">Home</h1>
+
+      <section className="mt-10 flex flex-col gap-10">
+        {
+          result.posts.length === 0 ? (
+            <p className="no-result">No Threads Found</p>
+          ) : (
+            <>
+              {
+                result.posts.map((post) => (
+                  <ThreadCard
+                    key={post._id}
+                    id={post._id}
+                    currentUserId={user?.id}
+                    parentId={post.parentId}
+                    content={post.text}
+                    author={post.author}
+                    community={post.community}
+                    createdAt={post.createAt}
+                    comments={post.children}
+                  />
+                ))
+              }
+            </>
+          )
+        }
+      </section>
+
     </div>
   )
 }
